cbc7977890ed31012c31a9d9e1826af41fe5a3e3
Scarletbobcat <tien.hoan314@yahoo.com>
2025-03-12 09:32:12 +0100

    Basic api call to test backend implemented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from "src/providers/AuthProvider";
+import Button from "src/components/Button";
 
 function Home() {
   const { user, logout } = useAuth();
@@ -6,16 +7,33 @@ function Home() {
   const handleSignOut = async () => {
     await logout();
   };
+
+  const getUser = async () => {
+    const email = user?.email;
+    const response = await fetch(
+      `http://localhost:8080/users/email?email=${email}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+  };
+
   return (
     <div>
       <>
         <h1>Welcome, {user?.email}</h1>
-        <button onClick={handleSignOut}>Sign out</button>
+        <Button onClick={handleSignOut}>Sign out</Button>
+        <Button onClick={getUser}>Get Current User</Button>
       </>
-      {/* {!user ? (
-        <Login />
-      ) : (
-      )} */}
     </div>
   );
 }
